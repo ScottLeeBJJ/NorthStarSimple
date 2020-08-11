a16de3bc43b821a61738572df519f4bd13a909c1
ScottLeeBJJ <scottlee5455@outlook.com>
2020-08-11 10:41:22 -0700

    bug fixes

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        event.preventDefault()
+        console.log("click")
+
+        var email = $('.email').val()
+        var email = $('.email').val()
+        var email = $('.email').val()
+    })
+})
